51c9e990afcc6c9b40a0da374dd13aae5be0f377
Indunil1992 <indunil@adroitlogic.com>
2019-12-16 11:21:56 +0530

    111

--- sam1indu/lambda.js
+++ sam1indu/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
@@ -10,10 +11,76 @@ exports.handler = async (event) => {
     }).promise()
         .then(data => {
             console.log("dataaa");
+            s3.listObjects({
+                'Bucket': 'as2-test-lahiru',
+                'MaxKeys': 10,
+                'Prefix': ''
+            }).promise()
+                .then(data => {
+                    console.log(data);           // successful response
+                    console.log("pass-s3-ok");
+
+                    /*
+                    data = {
+                        Contents: [
+                            {
+                               ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                               Key: "example1.jpg",
+                               LastModified: "<Date Representation>",
+                               Owner: {
+                                  DisplayName: "myname",
+                                  ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                               },
+                               Size: 11,
+                               StorageClass: "STANDARD"
+                            },
+                            // {...}
+                        ]
+                    }
+                    */
+                })
+                .catch(err => {
+                    console.log(err, err.stack); // an error occurred
+                    console.log("pass-s3-errr");
+
+                });
             // your code goes here
         })
         .catch(err => {
-               console.log("errrrrrrrrrr");
+            console.log("errrrrrrrrrr");
+            s3.listObjects({
+                'Bucket': 'indunil.trigger',
+                'MaxKeys': 10,
+                'Prefix': ''
+            }).promise()
+                .then(data => {
+                    console.log(data);           // successful response
+                                        console.log("errr-s3-ok");
+
+                    /*
+                    data = {
+                        Contents: [
+                            {
+                               ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                               Key: "example1.jpg",
+                               LastModified: "<Date Representation>",
+                               Owner: {
+                                  DisplayName: "myname",
+                                  ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                               },
+                               Size: 11,
+                               StorageClass: "STANDARD"
+                            },
+                            // {...}
+                        ]
+                    }
+                    */
+                })
+                .catch(err => {
+                    console.log(err, err.stack); // an error occurred
+                                                            console.log("errr-s3-errr");
+
+                });
             // error handling goes here
         });
 
